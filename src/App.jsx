ec54c81c9d46d933b47e95b63809e29803df3b3f
Tom Dent <tomdent991@hotmail.com>
2023-12-05 12:58:39 +0100

    form component complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-// import { useState } from 'react'
+import { useState } from 'react'
+import { uid } from 'uid';
 import './App.css'
 import Form from './components/Form'
 
 function App() {
+  const [activities, setActivities] = useState("");
+
+  function handleAddActivity(newActivity) {
+    setActivities([{ id: uid(), ...newActivity}, ...activities]);
+  }
 
   return (
     <>
       <h1>HIHIHI</h1>
-      <Form></Form>
+      <Form onAddActivity={handleAddActivity}></Form>
     </>
   )
 }
