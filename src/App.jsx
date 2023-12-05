798ffef1adb54fc73ea0e1f659fd652b6b3a85c1
Tom Dent <tomdent991@hotmail.com>
2023-12-05 20:16:53 +0100

    task complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
 import "./App.css";
@@ -6,8 +6,34 @@ import ActivityList from "./components/ActivityList";
 import Form from "./components/Form";
 
 function App() {
-  const [activities, setActivities] = useLocalStorageState("activity", {
-    defaultValue: [],
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [
+      {
+        id: uid(),
+        name: "Have a picnic",
+        isForGoodWeather: true,
+     }, {
+        id: uid(),
+        name: "Cook something nice",
+        isForGoodWeather: false,
+     }, {
+        id: uid(),
+        name: "Explore the neighbourhood",
+        isForGoodWeather: true,
+     }, {
+        id: uid(),
+        name: "Learn to code",
+        isForGoodWeather: false,
+     }, {
+        id: uid(),
+        name: "Do an ironman",
+        isForGoodWeather: true,
+     }, {
+        id: uid(),
+        name: "Read some Virginia Woolf",
+        isForGoodWeather: false,
+     }
+    ],
   });
   const [isGoodWeather, setIsGoodWeather] = useState(true);
   const [conditionEmoji, setConditionEmoji] = useState("");
@@ -15,21 +41,21 @@ function App() {
 
   function handleAddActivity(newActivity) {
     setActivities([{ id: uid(), ...newActivity }, ...activities]);
-    console.log(activities);
   }
 
-
+  useEffect(() => {
+    const intervalID = setInterval(fetchWeather, 5000);
+    fetchWeather();
+    return () => clearInterval(intervalID)
+  })
   
   async function fetchWeather() {
     const response = await fetch("https://example-apis.vercel.app/api/weather")
     const answer = await response.json();
-    console.log(answer.isGoodWeather);
-    console.log(answer);
     setIsGoodWeather(answer.isGoodWeather);
     setConditionEmoji(answer.condition);
     setTemperature(answer.temperature);
   }
-  fetchWeather();
 
   const filteredActivities = activities.filter((activity) => activity.isForGoodWeather === isGoodWeather);
 
@@ -37,13 +63,13 @@ function App() {
     setActivities(
       activities.filter((activity) => 
         activity.id !== id)
-    )
-    console.log(activities)
+    );
+
   }
 
   return (
     <>
-      <h1>Weather Activity Checker</h1>
+      <h1>Weather/Activity Checker</h1>
       <h2>{conditionEmoji}</h2>
       <h2>{temperature}&#176;C</h2>
       <ActivityList 
--- src/components/ActivityList/index.jsx
+++ src/components/ActivityList/index.jsx
@@ -10,9 +10,11 @@ export default function ActivityList({
 
   const headlineForGoodWeather = "The weather is awesome! Go outside and:"
   const headlineForBadWeather = "Bad weather outside! Here's what you can do:"
+  const forNoActivitiesGoodWeather = "There must be something you can do...it's so nice outside!"
+  const forNoActivitiesBadWeather = "There must be something you can do inside?"
 
   return (<>
-    <h3>{filter ? headlineForGoodWeather : headlineForBadWeather}</h3>
+    <h3>{activities.length === 0 ? filter ? forNoActivitiesGoodWeather : forNoActivitiesBadWeather : filter ? headlineForGoodWeather : headlineForBadWeather}</h3>
     <ul className="listContainer">
         {activities.map((activity) => (
           <li className="listItems" key={activity.id}>
