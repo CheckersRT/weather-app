f15234cc1d510ba14317bc5b68c1f87fe0271588
Tom Dent <tomdent991@hotmail.com>
2023-12-05 11:16:24 +0100

    form component works

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import { useState } from 'react'
 import './App.css'
+import Form from './components/Form'
 
 function App() {
 
   return (
     <>
       <h1>HIHIHI</h1>
+      <Form></Form>
     </>
   )
 }
