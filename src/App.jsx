194e131f8add98a5195351510a63be449674f074
Tom Dent <tomdent991@hotmail.com>
2023-12-05 17:52:28 +0100

    local storage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { uid } from "uid";
 import "./App.css";
 import Form from "./components/Form";
 import ActivityList from "./components/ActivityList";
+import useLocalStorageState from "use-local-storage-state";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useLocalStorageState("activity", {
+    defaultValue: [],
+  });
 
   function handleAddActivity(newActivity) {
     setActivities([{ id: uid(), ...newActivity }, ...activities]);
@@ -15,9 +18,7 @@ function App() {
   return (
     <>
       <h1>HIHIHI</h1>
-      <ActivityList
-        activities={activities}
-      ></ActivityList>
+      <ActivityList activities={activities}></ActivityList>
       <Form onAddActivity={handleAddActivity}></Form>
     </>
   );
